659090886469e73a2711f93c2a0f471c32daf9c0
David W. McKelvey <david@mckelveycreative.com>
2018-11-05 21:57:46 -0800

    importing data and rendering a day

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,73 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import { find } from 'lodash';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+import Layout from '../components/layout';
+import Image from '../components/image';
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-  </Layout>
-)
+class IndexPage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: '2008-12-06',
+    };
+  }
 
-export default IndexPage
+  renderDay() {
+    const { currentDate } = this.state;
+    const { edges } = this.props.data.allJson;
+    const { node: day } = find(edges, item => item.node.date === currentDate);
+    return (
+      <div>
+        <span className="count">{day.count}</span>
+        <span className="date">{currentDate}</span>
+      </div>
+    );
+  }
+
+  render() {
+    const { currentDate } = this.state;
+    return (
+      <Layout>
+        <h1>Hi people</h1>
+        <div data-date={currentDate} style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+          {this.renderDay()}
+          <Image />
+        </div>
+      </Layout>
+    );
+  }
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    edges: PropTypes.arrayOf(
+      PropTypes.shape({
+        node: PropTypes.shape({
+          count: PropTypes.string.isRequired,
+          date: PropTypes.string.isRequired,
+          fill: PropTypes.string.isRequired,
+        }).isRequired
+      })
+    ),
+  }).isRequired,
+};
+
+export const query = graphql`
+  query DecadeQuery {
+    allJson(
+      sort: {fields: [date], order: ASC},
+    ) {
+      edges {
+        node {
+          count
+          date
+          fill
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
